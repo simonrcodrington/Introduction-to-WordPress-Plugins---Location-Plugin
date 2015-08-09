669e277eb0e8369575ffd1ad36e0f42cbd9fced7
Simon Codrington <simon@codringtonitservices.com.au>
2015-08-09 10:35:14 +1000

    Updated plugin URI to point to github (before it had google.com)

--- wp_simple_location_plugin/wp_simple_location_plugin.php
+++ wp_simple_location_plugin/wp_simple_location_plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Wordpress Simple Location Plugin
-Plugin URI:  http://www.google.com.au
+Plugin URI:  https://github.com/simonrcodrington/Introduction-to-WordPress-Plugins---Location-Plugin
 Description: Creates an interfaces to manage store / business locations on your website. Useful for showing location based information quickly. Includes both a widget and shortcode for ease of use.
 Version:     1.0.0
 Author:      Simon Codrington
